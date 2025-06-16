617927a6d84125e549c361207b9910428eaff0e4
Eduar Sanchez <leosanchez_19@hotmail.com>
2025-06-16 08:38:01 -0500

    Merge pull request #3 from PoliedroSoftware/feature/health
    health

--- Poliedro.External.HealthCheck/Tolgee/TolgeeHealthCheckService.cs
+++ Poliedro.External.HealthCheck/Tolgee/TolgeeHealthCheckService.cs
@@ -13,6 +13,8 @@ public class TolgeeHealthCheckService(IHttpClientFactory httpClientFactory, ICon
         {
             var httpClient = httpClientFactory.CreateClient("TranslationCachingService");
             var response = await httpClient.GetAsync("translations?size=1");
+
+
             return response.IsSuccessStatusCode
                 ? HealthCheckResult.Healthy("Tolgee API is healthy.")
                 : HealthCheckResult.Unhealthy("Tolgee API is not healthy.");
