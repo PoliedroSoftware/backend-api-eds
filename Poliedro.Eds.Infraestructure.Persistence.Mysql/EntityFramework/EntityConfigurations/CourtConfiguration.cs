7e37d0944c456bcf5ceb816c51c9be74f3123fa4
Sebastian1401 <147678097+Sebastian1401@users.noreply.github.com>
2025-06-19 10:42:18 -0500

    Persistence Inventory

--- Poliedro.Eds.Infraestructure.Persistence.Mysql/EntityFramework/EntityConfigurations/CourtConfiguration.cs
+++ Poliedro.Eds.Infraestructure.Persistence.Mysql/EntityFramework/EntityConfigurations/CourtConfiguration.cs
@@ -22,12 +22,15 @@ public class CourtConfiguration
         builder.Property(x => x.Descripcion).HasColumnName("descripcion");
         builder.Property(x => x.Distintic).HasColumnName("distintic");
 
+        builder.HasOne(x => x.CourtInventory)
+                .WithOne()
+                .HasForeignKey<CourtInventoryEntity>(x => x.ReferenceId);
         builder.HasMany(x => x.CourtDispensers)
-       .WithOne()
-       .HasForeignKey(x => x.IdCourt);
+                .WithOne()
+                .HasForeignKey(x => x.IdCourt);
 
         builder.HasMany(x => x.CourtDocuments)
-               .WithOne()
+                .WithOne()
                .HasForeignKey(x => x.IdCourt);
 
         builder.HasMany(x => x.CourtExpenditures)
@@ -37,5 +40,7 @@ public class CourtConfiguration
         builder.HasMany(x => x.CourtTypeOfCollections)
                .WithOne()
                .HasForeignKey(x => x.IdCourt);
+        
+
     }
 }
