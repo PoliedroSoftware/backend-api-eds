a7ee71c27d077ea1c96ed1622b7f505ee3bf485a
jeider <jeidersilgado05@gmail.com>
2025-06-19 09:18:01 -0500

    add capacity and compartimet

--- Poliedro.Eds.Api/Controllers/v1/DashboardPowerBI/DashboardPowerBIContoller.cs
+++ Poliedro.Eds.Api/Controllers/v1/DashboardPowerBI/DashboardPowerBIContoller.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 using Poliedro.Eds.Application.DashboardPowerBI.Queries.GetAllDashboardPowerBI;
 using Poliedro.Eds.Application.Common.Features;
 using Poliedro.Eds.Domain.Common.Pagination;
+using Poliedro.Eds.Application.Capacity.Queries.GellAllCapacity;
+using Poliedro.Eds.Application.Compartiment.Queries.GellAllCompartiment;
 
 namespace Poliedro.Eds.Api.Controllers.v1.DashboardPowerBI;
     
@@ -11,16 +13,28 @@ namespace Poliedro.Eds.Api.Controllers.v1.DashboardPowerBI;
 [ApiController]
 public class DashboardPowerBI(IMediator mediator) : ControllerBase
 {
-    [HttpGet]
+   
+    [HttpGet("capacities")]
     public async Task<IActionResult> GetAll([FromQuery] PaginationParams paginationParams)
     {
-
-        var data = await mediator.Send(new GetAllDashboardPowerBIQuery(new PaginationParams { PageNumber = paginationParams.PageNumber, PageSize = paginationParams.PageSize }));
-
+        var data = await mediator.Send(new GellAllCapacityQuery(new PaginationParams { PageNumber = paginationParams.PageNumber, PageSize = paginationParams.PageSize }));
         if (data is null)
+        {
             return StatusCode(StatusCodes.Status404NotFound, ResponseApiService.Response(StatusCodes.Status404NotFound));
+        }
+        return StatusCode(StatusCodes.Status200OK, ResponseApiService.Response(StatusCodes.Status200OK, data));
+    }
 
-        return Ok(data);
+    [HttpGet("Compartiment")]
+    public async Task<ActionResult<IEnumerable<CompartimentDto>>> GetAll([FromQuery] PaginationParams paginationParams)
+    {
+        var data = await mediator.Send(new GellAllCompartimentQuery(new PaginationParams { PageNumber = paginationParams.PageNumber, PageSize = paginationParams.PageSize }));
+        if (data is null)
+        {
+            return StatusCode(StatusCodes.Status404NotFound, ResponseApiService.Response(StatusCodes.Status404NotFound));
+        }
+
+        return StatusCode(StatusCodes.Status200OK, ResponseApiService.Response(StatusCodes.Status200OK, data));
     }
 }
 
