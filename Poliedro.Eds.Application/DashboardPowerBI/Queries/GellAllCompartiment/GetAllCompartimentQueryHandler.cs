1ee821ebaeb1e81ee02fbe896d85503a831e5192
jeider <jeidersilgado05@gmail.com>
2025-06-19 11:11:09 -0500

    ok new enpoints

---
+++ Poliedro.Eds.Application/DashboardPowerBI/Queries/GellAllCompartiment/GetAllCompartimentQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MediatR;
+using Poliedro.Eds.Application.DashboardPowerBI.Dtos;
+using Poliedro.Eds.Domain.Compartiment.DomainCompartiment;
+
+namespace Poliedro.Eds.Application.DashboardPowerBI.Queries.GellAllCompartiment;
+public class GetAllCompartimentQueryHandler
+(
+    ICompartimentGetAllCompartiment compartimentDomainService,
+    IMapper mapper)
+    : IRequestHandler<GellAllCompartimentQuery, IEnumerable<CompartimentDto>>
+{
+    public async Task<IEnumerable<CompartimentDto>> Handle(GellAllCompartimentQuery request, CancellationToken cancellationToken)
+    {
+        var result = await compartimentDomainService.GetAllAsync(request.PaginationParams);
+        var ccc = mapper.Map<List<CompartimentDto>>(result);
+        return ccc;
+    }
+}
+
+
