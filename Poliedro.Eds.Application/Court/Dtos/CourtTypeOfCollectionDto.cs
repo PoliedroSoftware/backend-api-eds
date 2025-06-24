0a57793cb31b917b956dcbe1b20416f235c0781b
JOSE NAVARRO <janavarro724@gmail.com>
2025-06-24 08:40:48 -0500

    add

--- Poliedro.Eds.Application/Court/Dtos/CourtTypeOfCollectionDto.cs
+++ Poliedro.Eds.Application/Court/Dtos/CourtTypeOfCollectionDto.cs
@@ -6,6 +6,6 @@
         public int IdCourt { get; set; }
         public int IdTypeOfCollection { get; set; }
         public double Amount { get; set; }
-        public string? Description { get; set; }
+        public string Description { get; set; }
     }
 }
