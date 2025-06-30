ec49337d93d4f6d43054334c089388f5fdaaf429
jeider <jeidersilgado05@gmail.com>
2025-06-30 09:31:56 -0500

    new enpoint DashboardPowerBI

---
+++ Poliedro.Eds.Application/DashboardPowerBI/Queries/GellAllBusiness/GetAllBusinessQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MediatR;
+using Poliedro.Eds.Application.DashboardPowerBI.Dtos;
+using Poliedro.Eds.Domain.Business.DomainBusiness;
+
+namespace Poliedro.Eds.Application.DashboardPowerBI.Queries.GellAllBusiness;
+public class GetAllBusinessQueryHandler
+(
+    IBusinessGetAllService BusinessGetAllService,
+    IMapper mapper)
+    : IRequestHandler<GellAllBusinessQuery, IEnumerable<BusinessDto>>
+{
+    public async Task<IEnumerable<BusinessDto>> Handle(GellAllBusinessQuery request, CancellationToken cancellationToken)
+    {
+        var result = await BusinessGetAllService.GetAllAsync(request.PaginationParams);
+        return mapper.Map<List<BusinessDto>>(result);
+    }
+}
