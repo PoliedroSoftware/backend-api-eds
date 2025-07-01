39fa9d61ba129dcd3bffd95a2ead9e6312ebf010
Sebastian1401 <147678097+Sebastian1401@users.noreply.github.com>
2025-07-01 08:35:38 -0500

    Add Product price service in Shopping endpoint

--- Poliedro.Eds.Domain/Product/Entities/ProductEntity.cs
+++ Poliedro.Eds.Domain/Product/Entities/ProductEntity.cs
@@ -7,6 +7,6 @@ public class ProductEntity
     [Key]
     public int IdProduct { get; set; } = default!;
     public string Name { get; set; } = default!;
-    public int IdProductType { get; init; }
-    public double Price { get; init; }
+    public int IdProductType { get; set; }
+    public double Price { get; set; }
 }
