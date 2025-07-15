c5ec741c34f6be74cfdfef1b3a18ed519fbf692b
jeider <jeidersilgado05@gmail.com>
2025-07-14 20:36:06 -0500

    create Imp DashboardpowerBi

---
+++ Poliedro.Eds.Infraestructure.Persistence.Mysql/DashboardPowerBI/Impl/ProviderView/ProviderViewGetAllService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Poliedro.Eds.Domain.Common.Pagination;
+using Poliedro.Eds.Domain.Provider.DomainProvider;
+using Poliedro.Eds.Domain.Provider.Entities;
+using Poliedro.Eds.Infraestructure.Persistence.Mysql.Context;
+using Poliedro.Eds.Application.Ports.Redis;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.AspNetCore.Http;
+
+namespace Poliedro.Eds.Infraestructure.Persistence.Mysql.DashboardPowerBI.Impl.ProviderView;
+
+public class ProviderViewGetAllService(
+    ITenantDbContextFactory dbContextFactory,
+    IRedisService redisService,
+    IHttpContextAccessor httpContextAccessor
+    ) : IProviderGetAllService
+{
+    public async Task<IEnumerable<ProviderEntity>> GetAllAsync(PaginationParams paginationParams)
+    {
+        using var context = dbContextFactory.CreateDbContext();
+        var totalRows = await context.Provider.CountAsync();
+        var tenant = httpContextAccessor.HttpContext?.Items["tenant"]?.ToString();
+
+        string cacheKey = $"providerView:{paginationParams.PageNumber}:{paginationParams.PageSize}:{tenant}";
+        var cachedData = await redisService.GetCacheAsync<IEnumerable<ProviderEntity>>(cacheKey);
+        if (cachedData is not null) return cachedData;
+
+        var data = await context.Provider
+            .Skip((paginationParams.PageNumber - 1) * paginationParams.PageSize)
+            .Take(paginationParams.PageSize)
+            .ToListAsync();
+
+
+        await redisService.SetCacheAsync(cacheKey, data, TimeSpan.FromMinutes(1440));
+
+        return data;
+    }
+}
