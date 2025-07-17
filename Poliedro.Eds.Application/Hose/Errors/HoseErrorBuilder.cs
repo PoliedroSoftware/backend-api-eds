a98183d5ab6e499538565333bf5ef4852655200c
jeider <jeidersilgado05@gmail.com>
2025-07-17 16:39:38 -0500

    updated message error

--- Poliedro.Eds.Application/Hose/Errors/HoseErrorBuilder.cs
+++ Poliedro.Eds.Application/Hose/Errors/HoseErrorBuilder.cs
@@ -7,10 +7,15 @@ public class HoseErrorBuilder : IError
 {
     public const string HOSE_CREATION_ERROR = "HoseCreationErrorException";
     public const string HOSE_NOT_FOUND_ERROR = "HoseNotFoundErrorException";
+    public const string HOSE_LIMIT_REACHED_ERROR = "HoseLimitErrorException";
     public static Error HoseCreationException() => Error.CreateInstance(
        HOSE_CREATION_ERROR,
         "Failed to create Hose due to an internal error.",
         HttpStatusCode.InternalServerError);
+    public static Error HoseLimitErrorException() => Error.CreateInstance(
+      HOSE_CREATION_ERROR,
+       "Maximum number of hoses reached.",
+       HttpStatusCode.BadRequest);
 
     public const string HOSE_UPDATE_ERROR = "HoseUpdateErrorException";
 
