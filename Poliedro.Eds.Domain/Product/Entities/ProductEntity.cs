115b0495cd74c3b31e59f9404251f6202844a212
jeider <jeidersilgado05@gmail.com>
2025-07-18 13:20:31 -0500

    logic correction

--- Poliedro.Eds.Domain/Product/Entities/ProductEntity.cs
+++ Poliedro.Eds.Domain/Product/Entities/ProductEntity.cs
@@ -9,5 +9,4 @@ public class ProductEntity
     public string Name { get; set; } = default!;
     public int IdProductType { get; set; }
     public double Price { get; set; }
-    public DateTime Date { get; set; }
 }
