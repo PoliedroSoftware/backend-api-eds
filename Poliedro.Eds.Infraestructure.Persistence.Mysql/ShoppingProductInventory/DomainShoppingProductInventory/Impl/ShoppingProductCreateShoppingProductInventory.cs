2cccf917e896f9e9aa497aa86f39f06fd37735ec
JOSE NAVARRO <janavarro724@gmail.com>
2025-07-18 12:29:41 -0500

    Ok

--- Poliedro.Eds.Infraestructure.Persistence.Mysql/ShoppingProductInventory/DomainShoppingProductInventory/Impl/ShoppingProductCreateShoppingProductInventory.cs
+++ Poliedro.Eds.Infraestructure.Persistence.Mysql/ShoppingProductInventory/DomainShoppingProductInventory/Impl/ShoppingProductCreateShoppingProductInventory.cs
@@ -1,32 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore.Internal;
+﻿using Poliedro.Eds.Application.Ports.Redis;
 using Poliedro.Eds.Application.ShoppingProduct.Errors;
 using Poliedro.Eds.Domain.Common.Results;
 using Poliedro.Eds.Domain.Common.Results.Errors;
-using Poliedro.Eds.Domain.ShoppingProduct.DomainShoppingProduct;
-using Poliedro.Eds.Domain.ShoppingProduct.Entities;
 using Poliedro.Eds.Domain.ShoppingProductInventory.DomainShoppingProductInventory;
 using Poliedro.Eds.Domain.ShoppingProductInventory.Entities;
 using Poliedro.Eds.Infraestructure.Persistence.Mysql.Context;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Poliedro.Eds.Infraestructure.Persistence.Mysql.ShoppingProductInventory.DomainShoppingProductInventory.Impl
+namespace Poliedro.Eds.Infraestructure.Persistence.Mysql.ShoppingProductInventory.DomainShoppingProductInventory.Impl;
+
+public class ShoppingProductCreateShoppingProductInventory(
+    ITenantDbContextFactory dbContextFactory, 
+    IRedisService redisService) : IShoppingCreateShoppingProductInventory
 {
-    public class ShoppingProductCreateShoppingProductInventory(ITenantDbContextFactory dbContextFactory) : IShoppingCreateShoppingProductInventory
+    public async Task<Result<VoidResult, Error>> CreateAsync(ShoppingProductInventoryEntity shoppingProductEntity)
     {
-        public async Task<Result<VoidResult, Error>> CreateAsync(ShoppingProductInventoryEntity shoppingProductEntity)
-        {
-            //throw new NotImplementedException();
-            using var context = dbContextFactory.CreateDbContext();
-            await context.ShoppingProductInventory.AddAsync(shoppingProductEntity);
-            var result = await context.SaveChangesAsync() > 0;
-            if (!result)
-                return ShoppingProductErrorBuilder.ShoppingProductCreationException();
+        using var context = dbContextFactory.CreateDbContext();
+        await context.ShoppingProductInventory.AddAsync(shoppingProductEntity);
+        var result = await context.SaveChangesAsync() > 0;
+        if (!result)
+            return ShoppingProductErrorBuilder.ShoppingProductCreationException();
+        await redisService.RemoveByPrefixAsync("shoppingProductInventory:");
 
-            return VoidResult.Instance;
-        }
+        return VoidResult.Instance;
     }
 }
