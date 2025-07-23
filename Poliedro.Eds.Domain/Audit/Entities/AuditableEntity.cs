90d37c042b766d435ea886a60ed883b3b1122a4e
jeider <jeidersilgado05@gmail.com>
2025-07-23 13:34:25 -0500

    ok audit

--- Poliedro.Eds.Domain/Audit/Entities/AuditableEntity.cs
+++ Poliedro.Eds.Domain/Audit/Entities/AuditableEntity.cs
@@ -2,9 +2,9 @@
 {
     public abstract class AuditableEntity
     {
-        public string CreatedBy { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public string UpdatedBy { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public string? UpdatedBy { get; set; }
         public DateTime? UpdatedAt { get; set; }
     }
 
