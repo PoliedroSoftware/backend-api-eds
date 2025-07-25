70ff7e3f3625df6090b2728cd3dbba2c765650d8
Eduar Sanchez <leosanchez_19@hotmail.com>
2025-07-25 16:44:09 -0500

    ok test

--- Poliedro.Eds.Infraestructure.Persistence.Mysql/Court/Repositories/GetIdAuxService.cs
+++ Poliedro.Eds.Infraestructure.Persistence.Mysql/Court/Repositories/GetIdAuxService.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Internal;
 using Poliedro.Eds.Domain.Court.DomainService;
 using Poliedro.Eds.Domain.Court.Dto;
 using Poliedro.Eds.Infraestructure.Persistence.Mysql.Context;
