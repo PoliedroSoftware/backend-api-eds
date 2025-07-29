20e8b479fbe781f8fc1ebd36d459ba2f79993377
Eduar Sanchez <leosanchez_19@hotmail.com>
2025-07-29 10:35:52 -0500

    feat(whatsapp): add Service

--- WhatsApp/SendMessage/WhatsAppService.cs
+++ WhatsApp/SendMessage/WhatsAppService.cs
@@ -7,41 +7,35 @@ using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using Poliedro.Eds.Domain.SendMessage;
 
-namespace Poliedor.External.WhatsApp.SendMessage
+namespace Poliedor.External.WhatsApp.SendMessage;
+
+public class WhatsAppService (HttpClient httpClient, IConfiguration config)  : ISendMessage
 {
-    public class WhatsAppService (HttpClient httpClient, IConfiguration config)  : ISendMessage
+    public async Task SendMessageAsync(string phoneNumber, string message)
     {
-        private readonly string _whatsappApiUrl = config ["WhatsApp:Url"];
-        private readonly string _accessToken = "YOUR_ACCESS_TOKEN"; // Aquí colocas tu token de acceso de WhatsApp
-
-
-        public async Task SendMessageAsync(string phoneNumber, string message)
+        var requestPayload = new
         {
-            var requestPayload = new
+            messaging_product = "whatsapp",
+            recipient_type = "individual",
+            to = phoneNumber,
+            type = "text",
+            text = new
             {
-                messaging_product = "whatsapp",
-                recipient_type = "individual",
-                to = phoneNumber,
-                type = "text",
-                text = new
-                {
-                    preview_url = false,
-                    body = message
-                }
-            };
-
-            var jsonPayload = JsonConvert.SerializeObject(requestPayload);
-            var content = new StringContent(jsonPayload, Encoding.UTF8, "appsettings.json");
-
-            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {_accessToken}");
-
-            var response = await httpClient.PostAsync(_whatsappApiUrl, content);
-
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new Exception("Error sending message to WhatsApp API.");
+                preview_url = false,
+                body = message
             }
+        };
+
+        var jsonPayload = JsonConvert.SerializeObject(requestPayload);
+        var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+
+        httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {config["WhatsApp:Token"]}");
+
+        var response = await httpClient.PostAsync(config["WhatsApp:Url"], content);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception("Error sending message to WhatsApp API.");
         }
     }
-
 }
