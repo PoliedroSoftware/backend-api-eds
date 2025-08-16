32fe2dc16dad45d834ba15e6111a106b048ee9e3
Sebastian1401 <147678097+Sebastian1401@users.noreply.github.com>
2025-08-16 12:07:43 -0500

    Update CompartimentView for the ShoppingProduct menu in the app

--- Poliedro.Eds.Application/DashboardPowerBI/Dtos/CompartimentDto.cs
+++ Poliedro.Eds.Application/DashboardPowerBI/Dtos/CompartimentDto.cs
@@ -2,14 +2,16 @@ namespace Poliedro.Eds.Application.DashboardPowerBI.Dtos;
 
 public record CompartimentDto(
 string IdCompartment,
-string IdProduct,
-string IdBusiness,
 int Number,
 double Nominal,
 double Operative,
 double Stock,
 double Height,
+string IdProduct,
+string ProductName,
 string IdTank,
+string IdEds,
+string IdBusiness,
 DateOnly Date
 );
 
--- Poliedro.Eds.Infraestructure.Persistence.Mysql/EntityFramework/EntityConfigurations/DashboardPowerBI/CompartimentViewConfiguration.cs
+++ Poliedro.Eds.Infraestructure.Persistence.Mysql/EntityFramework/EntityConfigurations/DashboardPowerBI/CompartimentViewConfiguration.cs
@@ -11,14 +11,16 @@ public class CompartimentViewConfiguration
         builder.ToTable("v_compartiment");
         builder.HasKey(x => x.IdCompartment);
         builder.Property(x => x.IdCompartment).HasColumnName("id_compartiment");
-        builder.Property(x => x.IdBusiness).HasColumnName("id_business");
-        builder.Property(x => x.IdProduct).HasColumnName("id_product");
         builder.Property(x => x.Number).HasColumnName("number");
         builder.Property(x => x.Nominal).HasColumnName("nominal");
         builder.Property(x => x.Operative).HasColumnName("operative");
         builder.Property(x => x.Stock).HasColumnName("stock");
         builder.Property(x => x.Height).HasColumnName("height");
+        builder.Property(x => x.IdProduct).HasColumnName("id_product");
+        builder.Property(x => x.ProductName).HasColumnName("product_name");
         builder.Property(x => x.IdTank).HasColumnName("id_tank");
+        builder.Property(x => x.IdEds).HasColumnName("id_eds");
+        builder.Property(x => x.IdBusiness).HasColumnName("id_business");
         builder.Property(x => x.Date).HasColumnName("date");
     }
 }
