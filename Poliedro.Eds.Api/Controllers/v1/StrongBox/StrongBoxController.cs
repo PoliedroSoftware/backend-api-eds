94879c366c24effee3855cbb1fac42fe704411b8
William0814 <king-master90@hotmail.com>
2025-08-22 21:23:14 +0200

    Revert "First Commit"
    This reverts commit d8cb300aa071452ecefc8b003ce65d6336eb3b9f.
    
    Revert "First Commit"
    
    This reverts commit d8cb300aa071452ecefc8b003ce65d6336eb3b9f.

---
+++ Poliedro.Eds.Api/Controllers/v1/StrongBox/StrongBoxController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Poliedro.Eds.Api.Controllers.v1.StrongBox
+{
+    public class StrongBox : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Poliedro.Eds.Domain/StrongBox/Entities/StrongBoxEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poliedro.Eds.Domain.StrongBox.Entities
+{
+    internal class StrongBoxEntity
+    {
+    }
+}
