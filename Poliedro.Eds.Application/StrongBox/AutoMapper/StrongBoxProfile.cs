7a2f4579b7494fbd11077e7fbc938c74c72f726b
William0814 <king-master90@hotmail.com>
2025-09-01 21:57:27 +0200

    Add more changes

--- Poliedro.Eds.Application/StrongBox/AutoMapper/StrongBoxProfile.cs
+++ Poliedro.Eds.Application/StrongBox/AutoMapper/StrongBoxProfile.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using Poliedro.Eds.Application.StrongBox.Dtos;
+using Poliedro.Eds.Domain.StrongBox.Entities;
 
 namespace Poliedro.Eds.Application.StrongBox.AutoMapper;
 
@@ -11,7 +13,6 @@ public class StrongBoxProfile : Profile
 {
     public StrongBoxProfile()
     {
-        CreateMap<Domain.StrongBox.StrongBox, StrongBoxDto>();
-        CreateMap<StrongBoxDto, Domain.StrongBox.StrongBox>();
+        CreateMap<StrongBoxEntity, StrongBoxDto>().ReverseMap();
     }
 }
--- Poliedro.Eds.Application/StrongBox/Commands/StrongBoxCreateCommand.cs
+++ Poliedro.Eds.Application/StrongBox/Commands/StrongBoxCreateCommand.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MediatR;
+using Poliedro.Eds.Application.StrongBox.Dtos;
 
 namespace Poliedro.Eds.Application.StrongBox.Commands;
 
-public record StrongBoxCreateCommand(StrongBoxDtoCreateRequest Request) : IRequest<StrongBoxDto>;
+public record StrongBoxCreateCommand(StrongBoxDtoCreateRequest Request) : IRequest<Unit>;
