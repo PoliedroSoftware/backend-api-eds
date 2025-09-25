cdf99c356e2315b6b84719c0ba7978d882b7f34d
Erika <johannita.1125@gmail.com>
2025-09-25 09:38:23 -0500

    consumer error

--- Poliedro.Eds.Application/Compartiment/AutoMappers/CompartimentMapper.cs
+++ Poliedro.Eds.Application/Compartiment/AutoMappers/CompartimentMapper.cs
@@ -14,7 +14,7 @@ public class CompartimentMapper : Profile
         CreateMap<CompartimentEntity, CreateCompartimentCommand>().ReverseMap();
         CreateMap<CreateCompartimentRequestDto, CompartimentEntity>()
             .ForMember(dest => dest.Date, opt => opt.MapFrom(src => DateTime.UtcNow));
-        CreateMap<CompartimentEntity, CreateCompartimentRequestDto>().ReverseMap(); // Mantener el ReverseMap para otros usos si es necesario
+        CreateMap<CompartimentEntity, CreateCompartimentRequestDto>().ReverseMap();
         CreateMap<CompartimentEntity, UpdateCompartimentCommand>().ReverseMap();
     }
 }
