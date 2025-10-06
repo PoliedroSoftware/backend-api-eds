fff630c6eb36c0d06583cc3d977af0fe2b1be65a
William0814 <king-master90@hotmail.com>
2025-10-06 17:23:27 +0200

    feat: CreateIslanderCommandValidator: Create a best validator for every islander data.

--- Poliedro.Eds.Application/Islander/Commands/CreateIslander/CreateIslanderCommandValidator.cs
+++ Poliedro.Eds.Application/Islander/Commands/CreateIslander/CreateIslanderCommandValidator.cs
@@ -8,16 +8,29 @@ public class CreateIslanderCommandValidator : AbstractValidator<CreateIslanderRe
 {
     public CreateIslanderCommandValidator(IRedisService redisService)
     {
-        //RuleFor(x => x.Name)
-        //    .NotNull().WithMessage(redisService.GetValueFromCacheAsync("NameNotNull").GetAwaiter().GetResult())
-        //    .NotEmpty().WithMessage(redisService.GetValueFromCacheAsync("NameNotEmpty").GetAwaiter().GetResult());
+        RuleFor(x => x.Name)
+            .NotNull().WithMessage(redisService.GetValueFromCacheAsync("NameNotNull").GetAwaiter().GetResult())
+            .NotEmpty().WithMessage(redisService.GetValueFromCacheAsync("NameNotEmpty").GetAwaiter().GetResult());
 
-        //RuleFor(x => x.IdEds)
-        //    .NotNull().WithMessage(redisService.GetValueFromCacheAsync("IdEdsNotNull").GetAwaiter().GetResult())
-        //    .GreaterThan(0).WithMessage(redisService.GetValueFromCacheAsync("IdEdsNotEmpty").GetAwaiter().GetResult());
+        RuleFor(x => x.IdEds)
+            .NotNull().WithMessage(redisService.GetValueFromCacheAsync("IdEdsNotNull").GetAwaiter().GetResult())
+            .GreaterThan(0).WithMessage(redisService.GetValueFromCacheAsync("IdEdsNotEmpty").GetAwaiter().GetResult());
 
-        //RuleFor(x => x.Password)
-        //   .NotNull().WithMessage(redisService.GetValueFromCacheAsync("PasswordNotNull").GetAwaiter().GetResult())
-        //   .NotEmpty().WithMessage(redisService.GetValueFromCacheAsync("PasswordNotEmpty").GetAwaiter().GetResult());
+        RuleFor(x => x.Password)
+           .NotNull().WithMessage(redisService.GetValueFromCacheAsync("PasswordNotNull").GetAwaiter().GetResult())
+           .NotEmpty().WithMessage(redisService.GetValueFromCacheAsync("PasswordNotEmpty").GetAwaiter().GetResult());
+
+        RuleFor(x => x.Email)
+            .NotNull().WithMessage(redisService.GetValueFromCacheAsync("EmailNotNull").GetAwaiter().GetResult())
+            .NotEmpty().WithMessage(redisService.GetValueFromCacheAsync("EmailNotEmpty").GetAwaiter().GetResult());
+
+        RuleFor(x => x.FirstName)
+            .NotNull().WithMessage(redisService.GetValueFromCacheAsync("FirstNameNotNull").GetAwaiter().GetResult())
+            .NotEmpty().WithMessage(redisService.GetValueFromCacheAsync("FirstNameNotEmpty").GetAwaiter().GetResult());
+
+        RuleFor(x => x.LastName)
+            .NotNull().WithMessage(redisService.GetValueFromCacheAsync("LastNameNotNull").GetAwaiter().GetResult())
+            .NotEmpty().WithMessage(redisService.GetValueFromCacheAsync("LastNameNotEmpty").GetAwaiter().GetResult());
+
     }
 }
