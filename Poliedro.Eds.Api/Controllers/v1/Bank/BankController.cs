162b9aa1a920c8220e0642d5d9ca5e60374fe453
Eduar Sanchez <leosanchez_19@hotmail.com>
2025-10-09 15:22:16 -0500

    feat(bank): add logic bank transacction for court

--- Poliedro.Eds.Api/Controllers/v1/Bank/BankController.cs
+++ Poliedro.Eds.Api/Controllers/v1/Bank/BankController.cs
@@ -8,7 +8,6 @@ using Poliedro.Eds.Application.Bank.Dtos;
 using Poliedro.Eds.Application.Bank.Querys.BankGetAll;
 using Poliedro.Eds.Application.Bank.Querys.BankGetById;
 using Poliedro.Eds.Application.Bank.Querys.BankGetCurrentBalance;
-using Poliedro.Eds.Application.Bank.Querys.BankGetTotalBalance;
 using Poliedro.Eds.Application.Common.Features;
 using Swashbuckle.AspNetCore.Annotations;
 
@@ -87,36 +86,6 @@ public class BankController(IMediator mediator) : ControllerBase
             {
                 return NotFound(ResponseApiService.Response(StatusCodes.Status404NotFound, 
                     "Bank entry not found."));
-            }
-            
-            return Ok(ResponseApiService.Response(StatusCodes.Status200OK, result));
-        }
-        catch (Exception ex)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, 
-                ResponseApiService.Response(StatusCodes.Status500InternalServerError, ex.Message));
-        }
-    }
-
-    [SwaggerOperation(Summary = "Get Bank balance by Account ID")]
-    [SwaggerResponse(StatusCodes.Status200OK, "The operation was successful.", typeof(BankDto))]
-    [SwaggerResponse(StatusCodes.Status400BadRequest, "Incorrect request parameters.", typeof(ProblemDetails))]
-    [SwaggerResponse(StatusCodes.Status401Unauthorized, "The request lacks valid authentication credentials.", typeof(ProblemDetails))]
-    [SwaggerResponse(StatusCodes.Status404NotFound, "No bank entries found for the specified account.", typeof(ProblemDetails))]
-    [SwaggerResponse(StatusCodes.Status500InternalServerError, "Error processing the request.", typeof(ProblemDetails))]
-    [Authorize(Policy = "AdminOrIslander")]
-    [HttpGet("balance/{accountId}")]
-    public async Task<IActionResult> GetBalanceByAccount([FromRoute] int accountId)
-    {
-        try
-        {
-            var query = new BankGetTotalBalance(accountId);
-            var result = await mediator.Send(query);
-            
-            if (result == null)
-            {
-                return NotFound(ResponseApiService.Response(StatusCodes.Status404NotFound, 
-                    "No bank entries found for the specified account."));
             }
             
             return Ok(ResponseApiService.Response(StatusCodes.Status200OK, result));
