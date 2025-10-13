35dfe309b57577f31fc858b89799ffa1a0e9929e
Erika <johannita.1125@gmail.com>
2025-10-12 19:24:12 -0500

    purchasing

--- Poliedro.Eds.Application/Shopping/Commands/CreateShopping/CreateShoppingCommandValidator.cs
+++ Poliedro.Eds.Application/Shopping/Commands/CreateShopping/CreateShoppingCommandValidator.cs
@@ -8,6 +8,23 @@ namespace Poliedro.Eds.Application.Shopping.Shopping.CreateShopping;
 
 public class CreateShoppingCommandValidator : AbstractValidator<CreateShoppingRequestDto>
 {
-   
+    public CreateShoppingCommandValidator(IRedisService redisService)
+    {
+        //RuleFor(x => x.Invoice)
+        //    .NotEmpty().WithMessage(redisService.GetValueFromCacheAsync("InvoiceNotEmpty)").GetAwaiter().GetResult())
+        //    .MaximumLength(100).WithMessage(redisService.GetValueFromCacheAsync("InvoiceMaximumLength)").GetAwaiter().GetResult());
+
+        //RuleFor(x => x.Date)
+        //    .NotEmpty().WithMessage(redisService.GetValueFromCacheAsync("DateNotEmpty").GetAwaiter().GetResult())
+        //    .Must(date => date != default(DateTime)).WithMessage(redisService.GetValueFromCacheAsync("DateMust").GetAwaiter().GetResult());
+
+        //RuleFor(x => x.IdProvider)
+        //    .GreaterThan(0).WithMessage(redisService.GetValueFromCacheAsync("IdProviderGreaterThan").GetAwaiter().GetResult());
+
+        //RuleFor(x => x.IdCategory)
+        //    .GreaterThan(0).WithMessage(redisService.GetValueFromCacheAsync("IdCategoryGreaterThan").GetAwaiter().GetResult());
+
+        //RuleFor(x => x.Amount)
+        //    .GreaterThan(0).WithMessage(redisService.GetValueFromCacheAsync("AmountGreaterThan").GetAwaiter().GetResult());
     }
 }
