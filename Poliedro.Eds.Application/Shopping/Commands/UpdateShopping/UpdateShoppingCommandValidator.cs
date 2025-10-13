eb22fb07590ad3c3829757be44f44ec9b54c2f69
Erika <johannita.1125@gmail.com>
2025-10-12 19:36:42 -0500

    purchasing

--- Poliedro.Eds.Application/Shopping/Commands/UpdateShopping/UpdateShoppingCommandValidator.cs
+++ Poliedro.Eds.Application/Shopping/Commands/UpdateShopping/UpdateShoppingCommandValidator.cs
@@ -1,19 +1,50 @@
 using FluentValidation;
+using FluentValidation;
 using Poliedro.Eds.Application.Ports.Redis;
 using Poliedro.Eds.Application.Ports.Translations;
+using Poliedro.Eds.Domain.Provider.DomainProvider;
+using Poliedro.Eds.Domain.Common.Pagination;
+using Poliedro.Eds.Domain.Provider.Entities;
+using Newtonsoft.Json;
 
 namespace Poliedro.Eds.Application.Shopping.Commands.UpdateShopping;
 
 public class UpdateShoppingCommandValidator : AbstractValidator<UpdateShoppingCommand>
 {
-    public UpdateShoppingCommandValidator(IRedisService redisService)
+    private readonly IRedisService _redisService;
+    private readonly IProviderGetAllService _providerGetAllService;
+
+    public UpdateShoppingCommandValidator(IRedisService redisService, IProviderGetAllService providerGetAllService)
     {
+        _redisService = redisService;
+        _providerGetAllService = providerGetAllService;
+
         RuleFor(x => x.IdShopping)
-            .GreaterThan(0).WithMessage(redisService.GetValueFromCacheAsync("IdShoppingGreaterThan").GetAwaiter().GetResult());
+            .GreaterThan(0).WithMessage(_redisService.GetValueFromCacheAsync("IdShoppingGreaterThan").GetAwaiter().GetResult());
 
         RuleFor(x => x.Invoice)
-            .NotEmpty().WithMessage(redisService.GetValueFromCacheAsync("InvoiceNotEmpty").GetAwaiter().GetResult())
-            .MaximumLength(45).WithMessage(redisService.GetValueFromCacheAsync("InvoiceMaximumLength").GetAwaiter().GetResult());
+            .NotEmpty().WhenAsync(async (x, cancellationToken) =>
+            {
+                int? idProviderOtros = null;
+                var cachedId = await _redisService.GetValueFromCacheAsync("IdProviderOtros");
+                if (!string.IsNullOrEmpty(cachedId))
+                {
+                    idProviderOtros = int.Parse(cachedId);
+                }
+                else
+                {
+                    var allProviders = await _providerGetAllService.GetAllAsync(new PaginationParams { PageNumber = 1, PageSize = 1000 });
+                    var otrosProvider = allProviders.FirstOrDefault(p => p.Name.Equals("otros", StringComparison.OrdinalIgnoreCase));
+                    if (otrosProvider != null)
+                    {
+                        await _redisService.SetCacheAsync("IdProviderOtros", otrosProvider.IdProvider.ToString(), TimeSpan.FromDays(1));
+                        idProviderOtros = otrosProvider.IdProvider;
+                    }
+                }
+                return x.IdProvider != idProviderOtros;
+            })
+            .WithMessage(_redisService.GetValueFromCacheAsync("InvoiceNotEmpty").GetAwaiter().GetResult())
+            .MaximumLength(45).WithMessage(_redisService.GetValueFromCacheAsync("InvoiceMaximumLength").GetAwaiter().GetResult());
 
         RuleFor(x => x.Date)
             .NotEmpty().WithMessage(redisService.GetValueFromCacheAsync("DateNotEmpty").GetAwaiter().GetResult())
