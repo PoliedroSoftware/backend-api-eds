328f16ab8edd01cd8f87d37bc746cbbfd185b54c
JOSE NAVARRO <janavarro724@gmail.com>
2025-10-17 17:00:02 -0500

    feat: (transfer validation) post transfer validation

--- Poliedro.Eds.Application/TransferValidation/Dtos/TransferValidationDto.cs
+++ Poliedro.Eds.Application/TransferValidation/Dtos/TransferValidationDto.cs
@@ -18,11 +18,4 @@ public class TransferValidationDto
     
     public string? ConfirmedBy { get; set; }
     
-    public string? CreatedBy { get; set; }
-    
-    public DateTime? CreatedAt { get; set; }
-    
-    public string? UpdatedBy { get; set; }
-    
-    public DateTime? UpdatedAt { get; set; }
 }
