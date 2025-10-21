49bc384564872097c7c6c700ccc42d0593b7bcb9
Eduar Sanchez <leosanchez_19@hotmail.com>
2025-10-21 15:45:20 -0500

    feat(s3): rename appsettings

--- Poliedro.Eds.Application/Court/AutoMappers/CourtMapper.cs
+++ Poliedro.Eds.Application/Court/AutoMappers/CourtMapper.cs
@@ -9,41 +9,49 @@ using Poliedro.Eds.Domain.Court.DomainService;
 using Poliedro.Eds.Domain.Court.Entities;
 using Poliedro.Eds.Domain.Inventory.Entities;
 
-namespace Poliedro.Eds.Application.Court.AutoMappers
+namespace Poliedro.Eds.Application.Court.AutoMappers;
+
+public class CourtMapper : Profile
 {
-    public class CourtMapper : Profile
+    public CourtMapper()
     {
-        public CourtMapper()
-        {
-            CreateMap<CourtEntity, CreateCourtCommand>().ReverseMap();
-            CreateMap<CourtEntity, UpdateCourtCommand>().ReverseMap();
-            CreateMap<CourtEntity, CourtDto>().ReverseMap();
-            CreateMap<CourtDispenserEntity, CourtDispenserDto>().ReverseMap();
-            CreateMap<DocumentEntity, DocumentDto>().ReverseMap();
-            CreateMap<CourtExpenditureEntity, CourtExpenditureDto>().ReverseMap();
-            CreateMap<CourtTypeOfCollectionEntity, CourtTypeOfCollectionDto>().ReverseMap();
-            CreateMap<CourtDispenserEntity, CourtDispenserCommand>().ReverseMap();
-            CreateMap<CourtExpenditureEntity, CourtExpenditureCommand>().ReverseMap();
-            CreateMap<DocumentEntity, DocumentCommand>().ReverseMap();
-            CreateMap<CourtExpenditureEntity, CourtExpenditureCommand>().ReverseMap();
-            CreateMap<CourtTypeOfCollectionEntity, CourtTypeOfCollectionCommand>().ReverseMap();
-            CreateMap<CourtDispenserEntity, CourtDispenserSaleEntity>()
-             .ForMember(dest => dest.IdCompartiment, opt => opt.MapFrom(src => src.IdCompartiment))
-             .ForMember(dest => dest.IdProduct, opt => opt.MapFrom(src => src.IdProduct));
-            CreateMap<CourtDispenserCommand, CourtDispenserSaleEntity>()
-             .ForMember(dest => dest.GallonsDifferenceResult, opt => opt.MapFrom(src => src.GallonsDifferenceResult));
+        CreateMap<CourtEntity, CreateCourtCommand>().ReverseMap();
+        CreateMap<CourtEntity, UpdateCourtCommand>().ReverseMap();
+        CreateMap<CourtEntity, CourtDto>().ReverseMap();
+        CreateMap<CourtDispenserEntity, CourtDispenserDto>().ReverseMap();
+        CreateMap<DocumentEntity, DocumentDto>().ReverseMap();
+        CreateMap<CourtExpenditureEntity, CourtExpenditureDto>().ReverseMap();
+        CreateMap<CourtTypeOfCollectionEntity, CourtTypeOfCollectionDto>().ReverseMap();
+        CreateMap<CourtDispenserEntity, CourtDispenserCommand>().ReverseMap();
+        CreateMap<CourtExpenditureEntity, CourtExpenditureCommand>().ReverseMap();
 
-            CreateMap<InventoryEntity, InventoryDto>().ReverseMap();
-            CreateMap<InventoryEntity, InventoryCommand>().ReverseMap();
+        // Fix the DocumentCommand to DocumentEntity mapping
+        CreateMap<DocumentCommand, DocumentEntity>()
+        .ForMember(dest => dest.Descripcion, opt => opt.MapFrom(src => src.Descripcion))
+        .ForMember(dest => dest.DocumentName, opt => opt.MapFrom(src => src.DocumentName))
+        .ForMember(dest => dest.IdCourtDocument, opt => opt.Ignore()) // Auto-generated
+        .ForMember(dest => dest.IdCourt, opt => opt.Ignore()); // Will be set by EF relationships
 
-            CreateMap<CourtCollectionViewEntity, CourtCollectionViewDto>().ReverseMap();
-            CreateMap<CourtDispenserViewEntity, CourtDispenserViewDto>().ReverseMap();
-            CreateMap<CourtDocumentViewEntity, CourtDocumentViewDto>().ReverseMap();
-            CreateMap<CourtExpenditureViewEntity, CourtExpenditureViewDto>().ReverseMap();
-            CreateMap<CourtListResponseEntity, CourtListResponseDto>();
-            CreateMap<CourtViewEntity, CourtViewDto>().ReverseMap();
+        CreateMap<DocumentEntity, DocumentCommand>()
+        .ForMember(dest => dest.Descripcion, opt => opt.MapFrom(src => src.Descripcion))
+             .ForMember(dest => dest.DocumentName, opt => opt.MapFrom(src => src.DocumentName));
 
+        CreateMap<CourtExpenditureEntity, CourtExpenditureCommand>().ReverseMap();
+        CreateMap<CourtTypeOfCollectionEntity, CourtTypeOfCollectionCommand>().ReverseMap();
+        CreateMap<CourtDispenserEntity, CourtDispenserSaleEntity>()
+         .ForMember(dest => dest.IdCompartiment, opt => opt.MapFrom(src => src.IdCompartiment))
+         .ForMember(dest => dest.IdProduct, opt => opt.MapFrom(src => src.IdProduct));
+        CreateMap<CourtDispenserCommand, CourtDispenserSaleEntity>()
+         .ForMember(dest => dest.GallonsDifferenceResult, opt => opt.MapFrom(src => src.GallonsDifferenceResult));
 
-        }
+        CreateMap<InventoryEntity, InventoryDto>().ReverseMap();
+        CreateMap<InventoryEntity, InventoryCommand>().ReverseMap();
+
+        CreateMap<CourtCollectionViewEntity, CourtCollectionViewDto>().ReverseMap();
+        CreateMap<CourtDispenserViewEntity, CourtDispenserViewDto>().ReverseMap();
+        CreateMap<CourtDocumentViewEntity, CourtDocumentViewDto>().ReverseMap();
+        CreateMap<CourtExpenditureViewEntity, CourtExpenditureViewDto>().ReverseMap();
+        CreateMap<CourtListResponseEntity, CourtListResponseDto>();
+        CreateMap<CourtViewEntity, CourtViewDto>().ReverseMap();
     }
 }
