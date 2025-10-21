9a17ecaa13507e58a620f6c97c3e6db499554795
Eduar Leonardo Sanchez Pacheco <31712196+GitEduarSanchez@users.noreply.github.com>
2025-10-21 15:47:05 -0500

    feat(s3): rename appsettings (#232)

--- Poliedro.Eds.Application/Court/Dtos/DocumentDto.cs
+++ Poliedro.Eds.Application/Court/Dtos/DocumentDto.cs
@@ -3,9 +3,8 @@ namespace Poliedro.Eds.Application.Court.Dtos
     public class DocumentDto
     {
         public int IdCourtDocument { get; set; }
-
-        public string Descripcion { get; set; }
-
+        public string Descripcion { get; set; } = string.Empty;
+        public string? DocumentName { get; set; }
         public int IdCourt { get; set; }
     }
 }
