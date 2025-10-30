bcc9a522f0f470df87cb92f58f406cae2118471d
Eduar Sanchez <leosanchez_19@hotmail.com>
2025-10-30 16:43:22 -0500

    feat(shoppinng): add validatot for provider other

--- Poliedro.Eds.Application/Shopping/Commands/CreateShopping/CreateShoppingCommandValidator.cs
+++ Poliedro.Eds.Application/Shopping/Commands/CreateShopping/CreateShoppingCommandValidator.cs
@@ -1,8 +1,6 @@
 using FluentValidation;
 using Poliedro.Eds.Application.Ports.Redis;
-using Poliedro.Eds.Application.Ports.Translations;
 using Poliedro.Eds.Application.Shopping.Commands.CreateShopping;
-using Poliedro.Eds.Application.Shopping.Commands.UpdateShopping;
 using Poliedro.Eds.Domain.Provider.DomainProvider;
 
 namespace Poliedro.Eds.Application.Shopping.Shopping.CreateShopping;
@@ -16,36 +14,32 @@ public class CreateShoppingCommandValidator : AbstractValidator<CreateShoppingRe
         _providerService = providerService;
 
         RuleFor(x => x.Invoice)
-            .MustAsync(async (dto, invoice, cancellationToken) =>
-            {
-                // Get the provider to check if it's "Otros"
-                var providerResult = await _providerService.GetByIdAsync(dto.IdProvider);
-                if (providerResult.IsSuccess && providerResult.Value != null)
-                {
-                    // If provider is "Otros" (case insensitive), invoice is optional
-                    if (string.Equals(providerResult.Value.Name, "otros", StringComparison.OrdinalIgnoreCase))
-                    {
-                        return true; // Invoice is optional for "Otros" provider
-                    }
-                }
-                // For other providers, invoice is required
-                return !string.IsNullOrWhiteSpace(invoice);
-            })
-            .WithMessage(redisService.GetValueFromCacheAsync("InvoiceNotEmpty").GetAwaiter().GetResult())
-            .MaximumLength(45).WithMessage(redisService.GetValueFromCacheAsync("InvoiceMaximumLength").GetAwaiter().GetResult())
-            .When(x => !string.IsNullOrWhiteSpace(x.Invoice)); // Only validate length when invoice is provided
+          .MustAsync(async (dto, invoice, cancellationToken) =>
+             {
+                 var providerResult = await _providerService.GetByIdAsync(dto.IdProvider);
+                 if (providerResult.IsSuccess && providerResult.Value != null)
+                 {
+                     if (string.Equals(providerResult.Value.Name, "otros", StringComparison.OrdinalIgnoreCase))
+                     {
+                         return true;
+                     }
+                 }
+                 return !string.IsNullOrWhiteSpace(invoice);
+             }).WithMessage("La factura es requerida cuando el proveedor no es 'otros'")
+           .WithName("Invoice");
 
-        RuleFor(x => x.Date)
-            .NotEmpty().WithMessage(redisService.GetValueFromCacheAsync("DateNotEmpty").GetAwaiter().GetResult())
-            .Must(date => date != default(DateTime)).WithMessage(redisService.GetValueFromCacheAsync("DateMust").GetAwaiter().GetResult());
+        RuleFor(x => x.Date).NotEmpty().WithMessage("La fecha es requerida").WithName("Date");
 
         RuleFor(x => x.IdProvider)
-            .GreaterThan(0).WithMessage(redisService.GetValueFromCacheAsync("IdProviderGreaterThan").GetAwaiter().GetResult());
+            .GreaterThan(0).WithMessage("El ID del proveedor debe ser mayor a 0")
+            .WithName("IdProvider");
 
         RuleFor(x => x.IdCategory)
-            .GreaterThan(0).WithMessage(redisService.GetValueFromCacheAsync("IdCategoryGreaterThan").GetAwaiter().GetResult());
+             .GreaterThan(0).WithMessage("El ID de la categoría debe ser mayor a 0")
+             .WithName("IdCategory");
 
         RuleFor(x => x.Amount)
-            .GreaterThan(0).WithMessage(redisService.GetValueFromCacheAsync("AmountGreaterThan").GetAwaiter().GetResult());
+          .GreaterThan(0).WithMessage("El monto debe ser mayor a 0")
+          .WithName("Amount");
     }
 }
