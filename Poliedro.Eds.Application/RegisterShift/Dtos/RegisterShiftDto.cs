68e1913ddb123c0f124471735c2743ee785f8975
William0814 <king-master90@hotmail.com>
2025-11-01 17:18:07 +0100

    feat: RegisterShift: Update the new service with more fields in dataBase

--- Poliedro.Eds.Application/RegisterShift/Dtos/RegisterShiftDto.cs
+++ Poliedro.Eds.Application/RegisterShift/Dtos/RegisterShiftDto.cs
@@ -3,6 +3,8 @@ namespace Poliedro.Eds.Application.RegisterShift.Dtos;
 public class RegisterShiftDto
 {
     public int IdEds { get; set; }
+    public int IdIslander { get; set; }
+    public int IdBussines { get; set; }
     public DateOnly DateStartTime { get; set; }
     public TimeOnly StartTime { get; set; }
     public DateOnly DateEndTime { get; set; }
