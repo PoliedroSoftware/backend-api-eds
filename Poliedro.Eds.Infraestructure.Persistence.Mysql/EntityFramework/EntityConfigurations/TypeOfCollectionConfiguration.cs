a5b84e0282fe2a6cf3b079a87bffb6d58d64d7ed
Eduar Sanchez <leosanchez_19@hotmail.com>
2025-11-04 17:15:14 -0500

    feat(court): add get all

--- Poliedro.Eds.Infraestructure.Persistence.Mysql/EntityFramework/EntityConfigurations/TypeOfCollectionConfiguration.cs
+++ Poliedro.Eds.Infraestructure.Persistence.Mysql/EntityFramework/EntityConfigurations/TypeOfCollectionConfiguration.cs
@@ -9,7 +9,6 @@ public class TypeOfCollectionConfiguration
 {
     public TypeOfCollectionConfiguration(EntityTypeBuilder<TypeOfCollectionEntity> builder)
     {
-        // map to new table name
         builder.ToTable("type_of_collection");
         builder.HasKey(x => x.IdTypeOfCollection);
         builder.Property(x => x.IdTypeOfCollection).HasColumnName("id_type_of_collection");
