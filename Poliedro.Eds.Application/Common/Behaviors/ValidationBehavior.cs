d5308d2c5dc5e46bac28498e1f1884c96fd3021e
maikel <maikelvanegasholos3@gmail.com>
2025-12-03 10:14:22 -0500

    solution put en Compartment

--- Poliedro.Eds.Application/Common/Behaviors/ValidationBehavior.cs
+++ Poliedro.Eds.Application/Common/Behaviors/ValidationBehavior.cs
@@ -4,18 +4,22 @@ using MediatR;
 
 namespace Poliedro.Eds.Application.Common.Behaviors;
 
-public class ValidationBehaviour<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+public class ValidationBehaviour<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : notnull
 {
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         if (validators.Any())
         {
-            ValidationContext<TRequest> context = new ValidationContext<TRequest>(request);
-            List<ValidationFailure> list = (await Task.WhenAll(validators.Select((v) => v.ValidateAsync(context, cancellationToken)))).Where((r) => r.Errors.Any()).SelectMany((r) => r.Errors).ToList();
-            if (list.Count != 0)
-            {
-                throw new ValidationException(list);
-            }
+            var context = new ValidationContext<TRequest>(request);
+            var failures = (await Task.WhenAll(
+                    validators.Select(v => v.ValidateAsync(context, cancellationToken))))
+                .Where(r => r.Errors.Any())
+                .SelectMany(r => r.Errors)
+                .ToList();
+
+            if (failures.Count != 0)
+                throw new ValidationException(failures);
         }
 
         return await next();
